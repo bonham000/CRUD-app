2af7b75366079e6ab69166d8419bcab23df8148d
Sean Smith <sean.smith.2009@gmail.com>
2017-02-08 13:33:35 -0800

    Add Game feature implemented in Redux in client app

--- src/reducers/games.js
+++ src/reducers/games.js
@@ -1,10 +1,15 @@
 
-import { SET_GAMES } from '../actions/actions';
+import { SET_GAMES, ADD_GAME } from '../actions/actions';
 
 export default function games(state = [], action = {}) {
 	switch(action.type) {
 		case SET_GAMES:
 			return action.games;
+		case ADD_GAME:
+			return [
+				...state,
+				action.game
+			];
 		default: return state;
 	}
 }
