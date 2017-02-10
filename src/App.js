5d9d52d7ebc73df101d088fa358f72030c76140b
Sean Smith <sean.smith.2009@gmail.com>
2017-02-10 12:50:22 -0800

    updated react-router to beta and fixed active route class bug

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import { Link, Route } from 'react-router-dom';
 import GamesPage from './components/GamesPage';
 import GameFormPage from './components/GameFormPage';
 
+const ActiveLink = ({ label, to, activeOnlyWhenExact }) => (
+  <Route to={to} exact={activeOnlyWhenExact} children={({ match }) => {
+    return <Link className={to === match.url ? 'active item' : 'item'} to={to}>{label}</Link>
+  }}/>
+)
+
 export default class App extends Component {
   render() {
     return (
       <div className="ui container">
 
         <div className="ui three item menu">
-          <Link className="item" to="/">Home</Link>
-          <Link className="item" to="/games">Games</Link>
-          <Link className="item" to="/games/new">Add New Game</Link>
+          <ActiveLink activeOnlyWhenExact to="/" label="Home" />
+          <ActiveLink activeOnlyWhenExact to="/games" label="Games" />
+          <ActiveLink activeOnlyWhenExact to="/games/new" label="Add New Game" />
         </div>
 
         <Route exact={true} path="/games" component={GamesPage} />
